c6a68714a8f7cfd510aac0c7b4b070f9def99aea
Diego Rafael Guerrero <drafaelgue@hotmail.com>
2026-03-20 01:48:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1962,7 +1962,7 @@ with st.container(border=True):
         with st.container(border=True):
             st.markdown(f"### 📊 {tr('b3_plot_scale')}")
 
-            figS, axS = plt.subplots(figsize=(11.8, 5.2))
+            figS, axS = plt.subplots(figsize=(11.8, 4.9))
             figS.set_dpi(240)
             figS.patch.set_facecolor(BG)
             axS.set_facecolor(BG)
