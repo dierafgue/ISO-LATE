f474cc8629265ebf021c1692831a0277a5301200
Diego Rafael Guerrero <drafaelgue@hotmail.com>
2026-03-31 04:32:25 -0500

    Update funciones_usuario.py

--- funciones_usuario.py
+++ funciones_usuario.py
@@ -3047,7 +3047,7 @@ def plot_modelo_condensado_aislado(K_cond_ais, M_cond_ais, pisos_y):
 
     fig.tight_layout()
     _freeze_axes_limits(fig)
-    return fig
+    return figA
 
 # =============================================================================
 # === BLOQUE 6 (FUNCIONES): UTILIDADES THA / NEWMARK / RAYLEIGH / EXCEL ========
