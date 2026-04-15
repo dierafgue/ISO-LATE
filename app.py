51771814ca489a6641949b6f2044cd75452a6702
Diego Rafael Guerrero <drafaelgue@hotmail.com>
2026-04-15 16:32:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4493,10 +4493,11 @@ if a_ais_rel.shape[0] == n_pisos + 1:
     # solo superestructura (sin DOF 0 del aislador)
     m_sup = m_diag_ais[1:, :]
 
-    # usar directamente la aceleración de la superestructura
-    a_sup = a_ais_rel[1:, :]
+    # aceleración relativa de la superestructura respecto a la base aislada
+    a_base = a_ais_rel[0:1, :]
+    a_sup = a_ais_rel[1:, :] - a_base
 
-    # fuerzas inerciales por piso
+    # fuerzas inerciales por piso en la superestructura
     F_sup = m_sup * a_sup
 
     # cortantes acumulados por piso
