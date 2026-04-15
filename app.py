f2f15090f2f66750ecc974099d3de5c4728ba9d2
Diego Rafael Guerrero <drafaelgue@hotmail.com>
2026-04-15 16:11:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4493,8 +4493,8 @@ if a_ais_rel.shape[0] == n_pisos + 1:
     # solo superestructura (sin DOF 0 del aislador)
     m_sup = m_diag_ais[1:, :]
 
-    # aceleración absoluta de la superestructura (tipo ETABS)
-    a_sup = a_ais_rel[1:, :]
+    # aceleración absoluta de la superestructura
+    a_sup = a_ais_rel[1:, :] + ag_ais.reshape(1, -1)
 
     # fuerzas inerciales por piso
     F_sup = m_sup * a_sup
